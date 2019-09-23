3da3d5d9ef021b32046b9d093fe61c1e6c871641
Serena Gibbons <serenalgibbons@gmail.com>
2019-09-22 23:20:46 -0400

    Add Submission class

---
+++ ClassLibrary/ClassLibrary/Submission.cs
@@ -0,0 +1,71 @@
+﻿//*****************************************************************************
+// File: Submission.cs
+//
+// Purpose: Contains the class definition for Submission. This class is built to
+// be part of the ClassLibrary DLL.
+//
+// Written by: Serena Gibbons
+//
+// Compiler: Visual Studio 2017 
+//*****************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ClassLibrary
+{
+    class Submission
+    {
+        #region member variables
+        private string categoryName;
+        private string assignmentName;
+        private double grade;
+        #endregion
+
+        #region properties
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set { categoryName = value; }
+        }
+        public string AssignmentName
+        {
+            get { return assignmentName; }
+            set { assignmentName = value; }
+        }
+        public double Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        #endregion
+
+        #region methods
+        //*****************************************************************************
+        // Submission constructor
+        //
+        // Purpose: Default constructor used to instantiate the class Submission.
+        //*****************************************************************************
+        public Submission()
+        {
+            categoryName = null;
+            assignmentName = null;
+            grade = 0;
+        }
+        //*****************************************************************************
+        // Method: ToString
+        //
+        // Purpose: To override object's ToString method for the class Submission.
+        //*****************************************************************************
+        public override string ToString()
+        {
+            return "category name: " + categoryName
+                + "\nassignment name: " + assignmentName
+                + "\ngrade: " + grade;
+        }
+        #endregion
+    }
+}
