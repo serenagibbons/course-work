9e42ebcc83abfa355fefa57fdff9d7ba809900fc
Serena Gibbons <serenalgibbons@gmail.com>
2019-11-01 11:41:46 -0400

    Deserialize json file in OpenButton click handler; display course name and overall grade

--- CourseWorkGUI/CourseWorkGUI/MainWindow.xaml.cs
+++ CourseWorkGUI/CourseWorkGUI/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassLibrary;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace CourseWorkGUI
 {
@@ -21,6 +24,8 @@ namespace CourseWorkGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        CourseWork courseWork;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +37,31 @@ namespace CourseWorkGUI
             if (openFileDialog.ShowDialog() == true)
             {
                 string fileName = openFileDialog.FileName;
+
+                // display file path name
                 txtFilename.Text = fileName;
+
+                // instantiate couseWork
+                courseWork = new CourseWork();
+
+                // try reading from file, if exception thrown, break
+                try
+                {
+                    FileStream reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    DataContractJsonSerializer input;
+                    input = new DataContractJsonSerializer(typeof(CourseWork));
+                    courseWork = (CourseWork)input.ReadObject(reader);
+                    reader.Close();
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("Invalid file name.\n");
+                    return;
+                }
+
+                // display course name and overall grade
+                txtCourseName.Text = courseWork.CourseName;
+                txtOverallGrade.Text = Math.Round(courseWork.CalculateGrade(), 2).ToString();
             }
         }
     }
